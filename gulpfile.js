58fb01750980b4f6c9c4b8c879f5a04d697f9ee3
Christopher Leggett <christopher.leggett@icfi.com>
2018-07-24 10:46:24 -0400

    Adding gulp upload task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
+//load dependencies
 const del = require('del');
 const gulp = require('gulp');
 const zip = require('gulp-zip');
 const install = require('gulp-install');
 const sink = require('stream-sink');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
+//load properties
+const PROPERTIES_FILE = './properties/' + (process.env.PROPERTIES_FILE || 'local') + '.json';
+const properties = require(PROPERTIES_FILE);
+
+//set constants
 const BUILD_DIR = 'temp/build';
 const PACKAGE_NAME = 'o-daat.zip';
 const PACKAGE_FILES = ['package.json', 'package-lock.json', 'src/main/**'];
@@ -27,10 +35,38 @@ gulp.task('package', ['build'], () => {
         .pipe(gulp.dest(BUILD_DIR));
 });
 
+gulp.task('upload', ['package'], () => {
+    //ensure required properties
+    if (!properties['s3-bucket'] || !properties['s3-bucket'].match(/^[a-z0-9\.\-]+$/)) {
+        console.error(
+            'A valid s3-bucket must be defined to upload the lambda package.  ' +
+                'Please configure the "s3-bucket" property in "' + PROPERTIES_FILE + '".'
+        );
+        return;
+    }
+
+    //construct upload command
+    let uploadCmdString = 'sam package --template-file template.yaml --output-template-file ' +
+        BUILD_DIR + '/template.yaml --s3-bucket ' + properties['s3-bucket'];
+    if (properties.profile && properties.profile.match(/^[a-zA-Z0-9\-]+$/)) {
+        uploadCmdString += ' --profile ' + properties.profile;
+    }
+
+    //execute command
+    let uploadCmd = exec(uploadCmdString);
+    uploadCmd.then((value) => {
+        console.log(value.stdout);
+    }).catch((error) => {
+        //do nothing
+    });
+    return uploadCmd;
+});
+
 gulp.task('watch', () => {
     let watchTimeout = null,
         watchBuildRunning = false,
         watcher = gulp.watch(PACKAGE_FILES, (event) => {
+            //only queue up at most 1 build.
             if (!watchTimeout || watchBuildRunning) {
                 watchTimeout = setTimeout(() => {
                     watchBuildRunning = true;
@@ -42,3 +78,50 @@ gulp.task('watch', () => {
             }
         });
 });
+
+/*
+Development Requirements:
+    - Node v8.x.x
+    - Git
+
+Build Requirements:
+    - [All Development Requirements]
+    - Python
+    - AWS SAM CLI
+
+Local Testing Requirements:
+    - [All Build Requirements]
+    - Docker
+
+Deployment Requirements:
+    - [All Build Requirements]
+    - Access Key ID and Secret Access Key for target AWS account.
+    - AWS CLI installed and configured with appropriate Access Key ID and Secret Access Key.
+
+Development Steps:
+    - Ensure all development requirements have been met.
+    - Clone repository.
+    - Develop, commit and push changes.
+
+Build Steps:
+    - Ensure all build requirements have been met.
+    - Clone repository or pull latest.
+    - Run `npm install` from project root.
+    - Run `gulp package` from project root. TODO: Should package task automatically run unit tests.
+    - Find build Lambda package at 'temp/build/o-daat.zip'.
+
+Local Testing Steps:
+    - Ensure all local testing requirements have been met.
+    - Perform build by following build steps.
+    - Run `npm start` to start local testing container. TODO: Replace with native sam command or do something other than npm start script.
+    - Optionally Run `gulp watch` to automatically build and update local env.
+    - Test.
+
+Deployment Steps:
+    - Ensure all deployment requirements have been met.
+    - Ensure target S3 bucket is created (e.g. aws s3 mb s3://<bucket> --profile <profile>).  This is required to deploy the lambda package.
+    - Create new lambda package using `gulp package`.
+    - Package and upload the SAM application using `sam package --template-file template.yaml --output-template-file temp/build/template.yaml --s3-bucket <bucket> --profile <profile>` or `aws cloudformation package --template-file template.yaml --output-template-file temp/build/template.yaml --s3-bucket <bucket> --profile <profile>`.
+    - Deploy SAM application using `sam deploy --template-file temp/build/template.yaml --stack-name o-daat-stack --capabilities CAPABILITY_IAM --profile <profile>` or `aws cloudformation deploy --template-file temp/build/template.yaml --stack-name o-daat-stack --capabilities CAPABILITY_IAM --profile <profile>`.
+    - TODO: Additional details for deployment (e.g. Other AWS resources, access policies, etc.).
+*/
